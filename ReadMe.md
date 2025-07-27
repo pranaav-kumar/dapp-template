5d2b4a9db176ea78611eb3b1c04e911486408637
pranaav kumar <pranaavkumarj@gmail.com>
2025-07-27 13:23:01 +0530

    ok

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 steps:
 - run frontend : npm run dev
-- run backend : npm run deploy
+
+- run backend : npm run deploy (this is for only one time, only alter it when the code is changed)
 
 - paste the contract's ID inside the constants.js folder
--- frontend/constants.js
+++ frontend/constants.js
@@ -3,4 +3,4 @@ import SimpleAbi from "../backend/artifacts/contracts/Simple.sol/Simple.json";
 export const CONTRACT_ABI = SimpleAbi.abi;
 
 // Contract deployed address (update after every deploy)
-export const CONTRACT_ADDRESS = "0xeDb5a39DD6BC6bC6C24e4563292b68C7A65f835a";
+export const CONTRACT_ADDRESS = "0x402b0A0086e5a8C5197aA97eD898938163aC2C39";
